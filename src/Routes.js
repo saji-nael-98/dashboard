390cb0902464dcccb68536427d6556aa55cd1f5e
Saji Nael Zeer <saji.nael.98@gmail.com>
2022-09-18 00:41:32 +0300

    saji

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { RequireAuth } from 'react-auth-kit'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import SecureComponent from './components/SecureComponent'
+
+const RoutesComponent = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/login' } element={<Login/>}/>
+                <Route path={'/secure'} element={
+                  <RequireAuth loginPath={'/login'}>
+                    <SecureComponent/>
+                  </RequireAuth>
+                }/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesComponent
