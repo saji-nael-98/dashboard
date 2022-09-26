ef0c4132eb41f51cc8d53f48d70283805fc7f30f
Saji Nael Zeer <saji.nael.98@gmail.com>
2022-09-26 21:02:24 +0300

    saji-issue-5

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,8 +1,48 @@
+import Input from "antd/lib/input/Input";
 import React from "react";
+import { Button } from "../../components/UI/Button/Button";
 import DashboardPage from "../../components/UI/DashboardPage/DashboardPage";
+import { FormInput } from "../../components/UI/FormInput/FormInput";
+import Section from "../../components/UI/Section/Section";
+import CustomTable from "../../components/UI/Table/Table";
+import styles from "./Products.module.css";
 const Products = () => {
-  return <DashboardPage title='البضاعة'>
-            
-  </DashboardPage>;
+  const columns = [
+    {
+      title: "الاسم",
+      dataIndex: "الاسم",
+      key: "الاسم",
+    },
+    {
+      title: "الصنف",
+      dataIndex: "الصنف",
+      key: "الصنف",
+    },
+    {
+      title: "الكمية",
+      dataIndex: "الكمية",
+      key: "الكمية",
+    },
+    {
+      title: "الحالة",
+      dataIndex: "الحالة",
+      key: "الحالة",
+    },
+  ];
+  return (
+    <DashboardPage title="البضاعة">
+      <Section>
+        <div className={styles["filter"]}>
+          <div>
+            <Input placeholder="البحث عن منتج..."/>
+          </div>
+          <div>
+            <Button>اضافة جديد</Button>
+          </div>
+        </div>
+        <CustomTable columns={columns} dataSource={null} />
+      </Section>
+    </DashboardPage>
+  );
 };
 export default Products;
